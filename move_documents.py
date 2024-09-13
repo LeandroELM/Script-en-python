39ed85158c06c99df66344f4a428e65b13647a34
Leandro Lacayo <leandrolacayo17@gmail.com>
2024-09-13 16:49:57 -0600

    Implementacion de un diccionario para las rutas y una mejor concatenacion de las rutas de origen y destino

--- move_documents.py
+++ move_documents.py
@@ -1,38 +1,43 @@
+# Importa las librerias necesarias
 import os
 import shutil
 
-# Obetener el path actual de donde voy a mover los archivos
+# Obetener el path actual de donde voy a mover los archivos.
 current_path = os.getcwd()
 
-# Paths de destino para los archivos
-destiny_path_for_docx = r"C:\Users\leand\OneDrive - UNI\Escritorio\Word"
-destiny_path_for_xlsx = r"C:\Users\leand\OneDrive - UNI\Escritorio\Excel" 
-destiny_path_for_pdf = r"C:\Users\leand\OneDrive - UNI\Escritorio\PDF"
+# Diccionario de destino para los archivos.
+destination_path = {
+    ".xlsx": r"C:\Users\leand\OneDrive - UNI\Escritorio\Excel",
+    ".docx": r"C:\Users\leand\OneDrive - UNI\Escritorio\Word",
+    ".pdf": r"C:\Users\leand\OneDrive - UNI\Escritorio\PDF"
+}
 
-# Listar todos los archivos que hay dentro del path
+# Listar todos los archivos que hay dentro del path.
 archivos = os.listdir(current_path)
 
-# Funcion para mover todos los archivos
+# Funcion para mover todos los archivos.
 def move_documents(lista_de_archivos): 
-
-    # Recorrer todos los archivos buscando las extensiones: .xlsx | .docx | .pdf
-    try:
-        for archivo in lista_de_archivos:
-
-            if archivo.endswith(".xlsx"):
-                shutil.move(archivo, destiny_path_for_xlsx)
     
-            if archivo.endswith(".pdf"):
-                shutil.move(archivo, destiny_path_for_pdf)
+    # Recorre todos los archivos dentro de la lista.
+    for archivo in lista_de_archivos:
 
-            if archivo.endswith(".docx"):
-                shutil.move(archivo, destiny_path_for_docx)
+        # Extrae la extension de cada archivo de la lista.
+        extension = os.path.splitext(archivo)[1]
 
-        print("Todo ha ido bien")
-    
-    except:
-        pass
+        # Verifica si la extension esta dentro de las definidas en el diccionario.
+        if extension in destination_path:
+
+            try:
+                # Mueve los archivos a la carpeta de destino
+                shutil.move(os.path.join(current_path, archivo), destination_path[extension])
+                print("Todo ha salido bien :)")
+
+            # Imprime un error en caso de que lo haya
+            except NameError as e:
+
+                print(f"Ha ocurrido un error: {e.name}")
+                
 
 move_documents(archivos)
-    
+
 
